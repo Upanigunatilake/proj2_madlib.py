831df2fd270362cbcb5e9b76cb118dec404c392c
Upanigunatilake <upanigunatilake@gmail.com>
2021-05-16 12:06:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print('WELCOME TO MAD LIB LAB. LET US START PLAYING THIS AMAZING GAME.')
+print(' ENTER REQUIRED FIELDS AND SEE WHAT HAPPENS  NEXT')
+adj=input('Enter an adjective: ')
+verb1=input('Enter a verb: ')
+verb2=input('Enter a another verb: ')
+famous_person=input("Ener your favourite celebritie's name: ")
+madlib= f"Computer programming is so {adj} it makes me so exited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are a {famous_person}!"
+print(madlib)
+
